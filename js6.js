eaa5a2a13f0ba3df2093248a07df1aa22b1df05d
rajeshracha6 <rajeshracha752@gmail.com>
2024-09-28 11:19:36 +0530

    done

---
+++ js6.js
@@ -0,0 +1,133 @@
+
+// // Even numbers 1 to 10
+// function sum(a,b){
+//    
+//     for(i=a;i<b;i++){
+//         if(i%2==0){
+//         console.log(i);                  }
+//         }
+//       return sum;
+    
+//     }
+//       let a=2;
+//       let b=30;
+//     console.log(sum(a,b))
+//     console.log(sum(20,40))
+//     console.log(sum(50,100))
+
+// wraite a  pogram to print the square of each number from 1 to 10
+
+// function square(a,b){
+//     let sq=0;
+//     for(i=a;i<b;i++){
+        
+//             sq+=i*i
+
+
+//     }
+//     return sq;
+
+// }
+
+// let a=20;
+// let b=30;
+// console.log(square(a,b))
+
+
+// write a pogram to print the numbers from 1to 20 but skip the numbers that are divisible by 5
+// function number(a,b){
+//     for(i=a;i<b;i++){
+//         if(i%5===0){
+//             continue;
+//         }
+//         console.log(i);
+
+        
+
+// }
+// }
+// let a=1;
+// let b=20+1;
+// number(a,b)
+
+// write a pogram to print the numbers from 50 to 1 reverse order,but only print numbers that are divisible by 3
+
+// function number(a,b){ 
+//     for(i=a;i>b;i--){
+//         if(i%3===0){
+//             continue;
+//         }
+      
+//         console.log(i)
+// }
+// }
+
+// number(50,1)
+
+
+// write a program to find the sum of all numbers 1 to 30 that are divisible by 4
+
+// function sum(){         (dout)
+//     let sum=0;
+//     for(i=a;i<=b;i++){
+//         if(i%4===0){
+//             sum+=i;
+//         }
+
+//     }
+//     return sum;
+      
+// }
+
+// let a=1;
+// let b=10;
+// console.log(sum(a,b))
+
+// ARROWS:
+
+//  Even numbers 1 to 10
+
+
+// const numberr=(x,y)=>{
+//     for(let i=x;i<=y;i++){
+//         if(i%2===0){
+//             console.log(i);
+//           }
+//     }
+//     // return number;
+// }
+
+// let str1="madam";
+// function name(x,y){
+//     for(i=0;i<x.length;i++){
+//         console.log(i)
+
+
+//     }
+//     // return name;
+// }
+// name(str1);
+
+
+
+
+// numberr(1,10)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
